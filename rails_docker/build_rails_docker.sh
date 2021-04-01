8f9c7a3f32ea37453f587851ce1a2966f59df6dc
Soya Sano <soyasano2180plus1@gmail.com>
2021-04-01 21:05:49 +0900

    Fix build shell remove unneccesary line

--- rails_docker/build_rails_docker.sh
+++ rails_docker/build_rails_docker.sh
@@ -1,54 +1,35 @@
 #/bin/sh
 set -e
 
-# rubyの存在チェック
-if !(type "ruby" > /dev/null 2>&1); then
-    echo "rubyがインストールされていません"
-    exit 1
-fi
-
-# bundlerの存在チェック
-if !(type "bundle" > /dev/null 2>&1); then
-    echo "bundlerをインストールします。"
-    echo " ------------ START install bundler ------------"
-    gem install bundler
-    echo " ------------ END install bundler ------------"
-fi
-
 # Gemfile生成
-bundle init
-
-# gem railsのコメントを外す
-sed -i -e 's/# gem "rails"/gem "rails"/' Gemfile
-
-# 不要なファイル削除
-TMP_FILE="Gemfile-e" 
-if [[ -e $TMP_FILE ]]; then
-  echo "ファイルを削除します："$TMP_FILE
-  rm $TMP_FILE
-fi
+echo 'source "https://rubygems.org"' > Gemfile
+echo 'git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }' >> Gemfile
+echo 'gem "rails"' >> Gemfile
 
 # Gemfile.lock生成
 touch Gemfile.lock
 
-# bundle install
-echo " ------------ START Generate Gem ------------"
-bundle install --path=vendor/bundle
-echo " ------------ END Generate Gem ------------"
+echo " ------------ START Build 1------------"
+docker-compose build
+echo " ------------ END Build 1------------"
 
-
+# アプリの生成
 echo "アプリのビルドを開始します。"
 read -p "作成するアプリ名を入力してください：" AppName < /dev/tty
 
-# Docker構築開始
+# Railsファイル構築開始
 case ${AppName} in
+  '')
+    AppName=.;;
   *)
-    echo "コンテナの生成を開始します"
     echo " ------------ START Rails new ------------"
-    docker-compose run web rails new ${AppName} --force --no-deps --database=postgresql --skip-bundle
-    echo " ------------ END Rails new ------------";;
+    docker-compose run web rails new ${AppName} --force --no-deps --database=postgresql 
+    echo " ------------ END Rails new ------------"
 esac
 
-echo " ------------ START Build ------------"
+# annotate 追加
+echo 'gem "annotate"' >> Gemfile
+
+echo " ------------ START Build 2------------"
 docker-compose build
-echo " ------------ END Build------------"
+echo " ------------ END Build 2------------"
