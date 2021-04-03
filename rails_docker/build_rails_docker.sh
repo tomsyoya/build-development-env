5d955e890f152989bcedc214dfc2bd10a0c2d0f6
Soya Sano <soyasano2180plus1@gmail.com>
2021-04-03 17:51:31 +0900

    Fix Create DB Error

--- rails_docker/build_rails_docker.sh
+++ rails_docker/build_rails_docker.sh
@@ -10,35 +10,39 @@ echo 'gem "rails"' >> Gemfile
 touch Gemfile.lock
 
 echo " ------------ START Build 1------------"
-docker-compose build
+docker-compose run web bundle install
 echo " ------------ END Build 1------------"
 
 # アプリの生成
 echo "アプリのビルドを開始します。"
-read -p "作成するアプリ名を入力してください：" AppName < /dev/tty
 
 # Railsファイル構築開始
-case ${AppName} in
-  '')
-    AppName=.;;
-  *)
-    echo " ------------ START Rails new ------------"
-    docker-compose run web rails new ${AppName} --force --no-deps --database=postgresql 
-    echo " ------------ END Rails new ------------"
-
-    echo " ------------ START DB create ------------"
-    sed -i -e 's/#host: localhost/host: db/' ${AppName}/config/database.yml
-    SWAP_DB_FILE=${AppName}/config/database.yml-e
-    if [[ -e $SWAP_DB_FILE ]]; then
-       rm $SWAP_DB_FILE
-    fi
-     docker-compose run web rails db:create
-    echo " ------------ END DB create ------------"
-esac
-
-# annotate 追加
-echo 'gem "annotate"' >> Gemfile
+echo " ------------ START Rails new ------------"
+docker-compose run web rails new . --force --no-deps --database=postgresql 
+echo " ------------ END Rails new ------------"
 
 echo " ------------ START Build 2------------"
 docker-compose build
 echo " ------------ END Build 2------------"
+
+echo " ------------ START DB create ------------"
+# develop環境にDB接続情報を設定
+sed -i -e 's/#host: localhost/host: db/' ./config/database.yml
+sed -i -e 's/#port: 5432/port: 5432/' ./config/database.yml
+sed -i -e 's/#username: myapp/username: <%= ENV["DATABASE_USER"] %>/' ./config/database.yml
+sed -i -e 's/#password:/password: <%= ENV["DATABASE_PASSWORD"] %>/' ./config/database.yml
+# test環境にDB接続情報を設定
+sed -i -e 's/database: myapp_test/database: myapp_test\n  host: db\n  username: <%= ENV["DATABASE_USER"] %>\n  password: <%= ENV["DATABASE_PASSWORD"] %>/' ./config/database.yml
+SWAP_DB_FILE=./config/database.yml-e
+if [[ -e $SWAP_DB_FILE ]]; then
+   rm $SWAP_DB_FILE
+fi
+docker-compose run web rails db:create
+echo " ------------ END DB create ------------"
+
+# annotate 追加
+echo 'gem "annotate"' >> Gemfile
+
+echo " ------------ START Build 3------------"
+docker-compose build
+echo " ------------ END Build 3------------"
