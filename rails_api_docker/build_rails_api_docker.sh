d6801f00bf0d56bf6c63518ad778366cfce7a98c
Soya Sano <soyasano2180plus1@gmail.com>
2021-04-09 19:39:13 +0900

    Fix error

--- rails_api_docker/build_rails_api_docker.sh
+++ rails_api_docker/build_rails_api_docker.sh
@@ -14,7 +14,7 @@ echo "アプリのビルドを開始します。"
 
 # Railsファイル構築開始
 echo " ------------ START Rails new ------------"
-docker-compose run web rails new . --force --no-deps --api
+docker-compose run web rails new . --force --database=mysql --api
 echo " ------------ END Rails new ------------"
 
 # annotate 追加
@@ -24,6 +24,19 @@ echo " ------------ START APP BUILD ------------"
 docker-compose build
 echo " ------------ END APP BUILD ------------"
 
+echo " ------------ START DB create ------------"
+# develop環境にDB接続情報を設定
+sed -i -e 's/host: localhost/host: db/' ./config/database.yml
+sed -i -e 's/username: myapp/username: <%= ENV["DATABASE_USER"] %>/' ./config/database.yml
+sed -i -e 's/password:/password: <%= ENV["DATABASE_PASSWORD"] %>/' ./config/database.yml
+# # test環境にDB接続情報を設定
+sed -i -e 's/database: myapp_test/database: myapp_test\n  host: db\n  username: <%= ENV["DATABASE_USER"] %>\n  password: <%= ENV["DATABASE_PASSWORD"] %>/' ./config/database.yml
+SWAP_DB_FILE=./config/database.yml-e
+if [[ -e $SWAP_DB_FILE ]]; then
+   rm $SWAP_DB_FILE
+fi
+docker-compose run web rails db:create
+echo " ------------ END DB create ------------"
 
 # 当シェルをgit管理から除外
 echo "build_rails_api_docker.sh" >> .gitignore
